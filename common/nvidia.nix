291d39035bd27d891a4111620ac965357097ec79
Daniel Proctor <dan@danielproctor.dev>
2023-07-03 00:35:36 +0100

    merge hardware

--- common/nvidia.nix
+++ common/nvidia.nix
@@ -1,13 +1,5 @@
 { config, lib, pkgs, ... }:
 {
-  # NVIDIA
-  # Make sure opengl is enabled
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
   # NVIDIA drivers are unfree.
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
@@ -17,17 +9,25 @@
   # Tell Xorg to use the nvidia driver
   services.xserver.videoDrivers = ["nvidia"];
 
-  hardware.nvidia = {
-    # Fix graphical corruption on suspend/resume
-    powerManagement.enable = true;
-    # Modesetting is needed for most wayland compositors
-    modesetting.enable = true;
-    # Use the open source version of the kernel module
-    # Only available on driver 515.43.04+
-    open = true;
-    # Enable the nvidia settings menu
-    nvidiaSettings = true;
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  hardware = {
+    # Make sure opengl is enabled
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+    nvidia = {
+      # Fix graphical corruption on suspend/resume
+      powerManagement.enable = true;
+      # Modesetting is needed for most wayland compositors
+      modesetting.enable = true;
+      # Use the open source version of the kernel module
+      # Only available on driver 515.43.04+
+      open = true;
+      # Enable the nvidia settings menu
+      nvidiaSettings = true;
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
   };
 }
